c39c9c0d237307eb54f216addb255cfc95a51ff4
melk0sha <melk0shadev@gmail.com>
2019-11-05 00:35:42 +0300

    feat: add rus in data.js and fully complete index.js

--- scripts/data.js
+++ scripts/data.js
@@ -385,316 +385,379 @@ const buttonsRus = [
     {
         content: ['ё', 'Ё'],
         row: 0,
+        classNames: ['key-common', 'changeable'],
         keyCode: 192
     },
     {
         content: ['1', '!'],
+        classNames: ['key-common', 'changeable'],
         row: 0,
         keyCode: 49
     },
     {
         content: ['2', '\"'],
+        classNames: ['key-common', 'changeable'],
         row: 0,
         keyCode: 50
     },
     {
         content: ['3', '№'],
+        classNames: ['key-common', 'changeable'],
         row: 0,
         keyCode: 51
     },
     {
         content: ['4', ';'],
+        classNames: ['key-common', 'changeable'],
         row: 0,
         keyCode: 52
     },
     {
         content: ['5', '%'],
+        classNames: ['key-common', 'changeable'],
         row: 0,
         keyCode: 53
     },
     {
         content: ['6', ':'],
+        classNames: ['key-common', 'changeable'],
         row: 0,
         keyCode: 54
     },
     {
         content: ['7', '?'],
+        classNames: ['key-common', 'changeable'],
         row: 0,
         keyCode: 55
     },
     {
         content: ['8', '*'],
+        classNames: ['key-common', 'changeable'],
         row: 0,
         keyCode: 56
     },
     {
         content: ['9', '('],
+        classNames: ['key-common', 'changeable'],
         row: 0,
         keyCode: 57
     },
     {
         content: ['0', ')'],
+        classNames: ['key-common', 'changeable'],
         row: 0,
         keyCode: 48
     },
     {
         content: ['-', '_'],
+        classNames: ['key-common', 'changeable'],
         row: 0,
         keyCode: 189
     },
     {
         content: ['=', '+'],
+        classNames: ['key-common', 'changeable'],
         row: 0,
         keyCode: 187
     },
     {
         content: ['Backspace'],
+        classNames: ['key-common', 'backspace'],
         row: 0,
         keyCode: 8
     },
     {
         content: ['Tab'],
+        classNames: ['key-common', 'tab'],
         row: 1,
         keyCode: 9
     },
     {
         content: ['й', 'Й'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 81
     },
     {
         content: ['ц', 'Ц'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 87
     },
     {
         content: ['у', 'У'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 69
     },
     {
         content: ['к', 'К'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 82
     },
     {
         content: ['е', 'Е'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 84
     },
     {
         content: ['н', 'Н'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 89
     },
     {
         content: ['г', 'Г'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 85
     },
     {
         content: ['ш', 'Ш'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 73
     },
     {
         content: ['щ', 'Щ'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 79
     },
     {
         content: ['з', 'З'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 80
     },
     {
         content: ['х', 'Х'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 219
     },
     {
         content: ['ъ', 'Ъ'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 221
     },
     {
         content: ['\\', '/'],
+        classNames: ['key-common', 'changeable'],
         row: 1,
         keyCode: 220
     },
     {
         content: ['CapsLock'],
+        classNames: ['key-common', 'capslock'],
         row: 2,
         keyCode: 20
     },
     {
         content: ['ф', 'Ф'],
+        classNames: ['key-common', 'changeable'],
         row: 2,
         keyCode: 65
     },
     {
         content: ['ы', 'Ы'],
+        classNames: ['key-common', 'changeable'],
         row: 2,
         keyCode: 83
     },
     {
         content: ['в', 'В'],
+        classNames: ['key-common', 'changeable'],
         row: 2,
         keyCode: 68
     },
     {
         content: ['а', 'А'],
+        classNames: ['key-common', 'changeable'],
         row: 2,
         keyCode: 70
     },
     {
         content: ['п', 'П'],
+        classNames: ['key-common', 'changeable'],
         row: 2,
         keyCode: 71
     },
     {
         content: ['р', 'Р'],
+        classNames: ['key-common', 'changeable'],
         row: 2,
         keyCode: 72
     },
     {
         content: ['о', 'О'],
+        classNames: ['key-common', 'changeable'],
         row: 2,
         keyCode: 74
     },
     {
         content: ['л', 'Л'],
+        classNames: ['key-common', 'changeable'],
         row: 2,
         keyCode: 75
     },
     {
         content: ['д', 'Д'],
+        classNames: ['key-common', 'changeable'],
         row: 2,
         keyCode: 76
     },
     {
         content: ['ж', 'Ж'],
+        classNames: ['key-common', 'changeable'],
         row: 2,
         keyCode: 186
     },
     {
         content: ['э', 'Э'],
+        classNames: ['key-common', 'changeable'],
         row: 2,
         keyCode: 222
     },
     {
         content: ['Enter'],
+        classNames: ['key-common', 'enter'],
         row: 2,
         keyCode: 13
     },
     {
         content: ['Shift'],
+        classNames: ['key-common', 'shift-left'],
         row: 3,
         keyCode: 16
     },
     {
         content: ['я', 'Я'],
+        classNames: ['key-common', 'changeable'],
         row: 3,
         keyCode: 90
     },
     {
         content: ['ч', 'Ч'],
+        classNames: ['key-common', 'changeable'],
         row: 3,
         keyCode: 88
     },
     {
         content: ['с', 'С'],
+        classNames: ['key-common', 'changeable'],
         row: 3,
         keyCode: 67
     },
     {
         content: ['м', 'М'],
+        classNames: ['key-common', 'changeable'],
         row: 3,
         keyCode: 86
     },
     {
         content: ['и', 'И'],
+        classNames: ['key-common', 'changeable'],
         row: 3,
         keyCode: 66
     },
     {
         content: ['т', 'Т'],
+        classNames: ['key-common', 'changeable'],
         row: 3,
         keyCode: 78
     },
     {
         content: ['ь', 'Ь'],
+        classNames: ['key-common', 'changeable'],
         row: 3,
         keyCode: 77
     },
     {
         content: ['б', 'Б'],
+        classNames: ['key-common', 'changeable'],
         row: 3,
         keyCode: 188
     },
     {
         content: ['ю', 'Ю'],
+        classNames: ['key-common', 'changeable'],
         row: 3,
         keyCode: 190
     },
     {
         content: ['.', ','],
+        classNames: ['key-common', 'changeable'],
         row: 3,
         keyCode: 191
     },
     {
         content: ['↑'],
+        classNames: ['key-common', 'arrow-up'],
         row: 3,
         keyCode: 38
     },
     {
         content: ['Shift'],
+        classNames: ['key-common', 'shift-right'],
         row: 3,
         keyCode: 16
     },
     {
         content: ['Ctrl'],
+        classNames: ['key-common', 'ctrl-left'],
         row: 4,
         keyCode: 17
     },
     {
         content: ['Win'],
+        classNames: ['key-common', 'win'],
         row: 4,
         keyCode: 91
     },
     {
         content: ['Alt'],
+        classNames: ['key-common', 'alt-left'],
         row: 4,
         keyCode: 18
     },
     {
         content: [' '],
+        classNames: ['key-common', 'space'],
         row: 4,
         keyCode: 32
     },
     {
         content: ['Alt'],
+        classNames: ['key-common', 'alt-right'],
         row: 4,
         keyCode: 18
     },
     {
-        content: ['Ctrl'],
-        row: 4,
-        keyCode: 17
-    },
-    {
         content: ['←'],
+        classNames: ['key-common', 'arrow-left'],
         row: 4,
         keyCode: 37
     },
     {
         content: ['↓'],
+        classNames: ['key-common', 'arrow-down'],
         row: 4,
         keyCode: 40
     },
     {
         content: ['→'],
+        classNames: ['key-common', 'arrow-right'],
         row: 4,
         keyCode: 39
+    },
+    {
+        content: ['Ctrl'],
+        classNames: ['key-common', 'ctrl-right'],
+        row: 4,
+        keyCode: 17
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,13 @@
-let isShift = false;
+let languageData = 'eng',
+    isShift = false,
+    isCapsLock = false;
+
+if (sessionStorage.getItem('language') == null) {
+    sessionStorage.setItem('language', languageData);
+} else {
+    languageData = sessionStorage.getItem('language');
+}
+
 let container = document.createElement('div');
 container.classList.add('container');
 document.body.appendChild(container);
@@ -28,16 +37,74 @@ let row4 = document.createElement('div');
 row4.classList.add('row');
 keyboard.appendChild(row4);
 
+textarea.autofocus = true;
+textarea.onblur = () => textarea.focus();
+
 buttonsEng.map(button => {
     const buttonElement = document.createElement('button');
+    let counterSpans = 0;
     button.content.map(spanContent => {
         const span = document.createElement('span');
         span.textContent = spanContent;
+        if (button.classNames.includes('changeable') && counterSpans == 0) {
+            span.classList.add('down');
+            counterSpans++;
+        } else if (button.classNames.includes('changeable') && counterSpans == 1) {
+            span.classList.add('up');
+        }
         buttonElement.appendChild(span);
     });
     button.classNames.forEach(classElementName => {
         buttonElement.classList.add(classElementName);
+        if (languageData == 'eng') {
+            buttonElement.classList.add('lang');
+        } else {
+            buttonElement.classList.add('change-lang');
+        }
     });
+    buttonElement.dataset.keyCode = button.keyCode;
+    switch (button.row) {
+        case 0:
+            row0.appendChild(buttonElement);
+            break;
+        case 1:
+            row1.appendChild(buttonElement);
+            break;
+        case 2:
+            row2.appendChild(buttonElement);
+            break;
+        case 3:
+            row3.appendChild(buttonElement);
+            break;
+        case 4:
+            row4.appendChild(buttonElement);
+            break;
+    }
+});
+
+buttonsRus.map(button => {
+    const buttonElement = document.createElement('button');
+    let counterSpans = 0;
+    button.content.map(spanContent => {
+        const span = document.createElement('span');
+        span.textContent = spanContent;
+        if (button.classNames.includes('changeable') && counterSpans == 0) {
+            span.classList.add('down');
+            counterSpans++;
+        } else if (button.classNames.includes('changeable') && counterSpans == 1) {
+            span.classList.add('up');
+        }
+        buttonElement.appendChild(span);
+    });
+    button.classNames.forEach(classElementName => {
+        buttonElement.classList.add(classElementName);
+        if (languageData == 'rus') {
+            buttonElement.classList.add('lang');
+        } else {
+            buttonElement.classList.add('change-lang');
+        }
+    });
+    buttonElement.dataset.keyCode = button.keyCode;
     switch (button.row) {
         case 0:
             row0.appendChild(buttonElement);
@@ -58,23 +125,147 @@ buttonsEng.map(button => {
 });
 
 keyboard.addEventListener('click', event => {
-    console.log(event.target);
     let currentButton = event.target;
     if (currentButton.tagName == 'BUTTON') {
         let spans = currentButton.children;
-        if (currentButton.classList.value.includes('changeable') || currentButton.classList.value.includes('space')) {
-            if (isShift) {
-                textarea.value += spans[1].textContent;
-            } else {
-                textarea.value += spans[0].textContent;
-            }
+        if (currentButton.classList.contains('changeable')) {
+            let span = currentButton.querySelector('span.down');
+            textarea.value += span.textContent;
         } else if (currentButton.classList.value.includes('arrow')) {
             textarea.value += spans[0].textContent;
+        } else if (currentButton.classList.contains('tab')) {
+            textarea.value += '        ';
+        } else if (currentButton.classList.contains('space')) {
+            textarea.value += spans[0].textContent;
+        } else if (currentButton.classList.contains('backspace')) {
+            textarea.value = textarea.value.slice(0, -1);
+        } else if (currentButton.classList.contains('enter')) {
+            textarea.value += '\n';
         }
-        
-        console.log(currentButton.classList.value.includes('changeable'));
-    }
-    else if (currentButton.tagName == 'SPAN' && currentButton.parentElement.classList.value.includes('changeable')) {
+    } else if (currentButton.tagName == 'SPAN' && currentButton.parentElement.classList.contains('changeable')) {
         textarea.value += currentButton.textContent;
+    } else if (currentButton.tagName == 'SPAN' && currentButton.parentElement.classList.value.includes('arrow')) {
+        textarea.value += currentButton.textContent;
+    } else if (currentButton.tagName == 'SPAN' && currentButton.parentElement.classList.contains('tab')) {
+        textarea.value += '        ';
+    } else if (currentButton.tagName == 'SPAN' && currentButton.parentElement.classList.contains('space')) {
+        textarea.value += currentButton.textContent;
+    } else if (currentButton.tagName == 'SPAN' && currentButton.parentElement.classList.contains('backspace')) {
+        textarea.value = textarea.value.slice(0, -1);
+    } else if (currentButton.tagName == 'SPAN' && currentButton.parentElement.classList.contains('enter')) {
+        textarea.value += '\n';
     }
 });
+
+let pressed = new Set();
+
+document.addEventListener('keydown', event => {
+    const rowElements = [...keyboard.children];
+    rowElements.forEach(rowElement => {
+        const buttonElements = [...rowElement.querySelectorAll('.lang')];
+        let buttonFound = buttonElements.find(button => event.keyCode == button.dataset.keyCode);
+        if (buttonFound) {
+            if (event.code == 'ShiftRight') {
+                buttonFound = buttonElements.find(button => button.classList.contains('shift-right'));
+            } else if (event.code == 'AltRight') {
+                buttonFound = buttonElements.find(button => button.classList.contains('alt-right'));
+            } else if (event.code == 'ControlRight') {
+                buttonFound = buttonElements.find(button => button.classList.contains('ctrl-right'));
+            }
+            buttonFound.classList.add('active');
+            if (buttonFound.dataset.keyCode == 16 || buttonFound.dataset.keyCode == 20) {
+                isShift = true;
+                isCapsLock = !isCapsLock;
+                const rowElements = [...keyboard.children];
+                rowElements.forEach(rowElement => {
+                    const buttonElements = [...rowElement.querySelectorAll('.lang')];
+                    buttonElements.forEach(buttonElement => {
+                        if (buttonElement.classList.contains('changeable')) {
+                            const spans = [...buttonElement.children];
+                            let temp = null;
+                            temp = spans[0].classList.value;
+                            spans[0].classList.remove(spans[0].classList.value);
+                            spans[0].classList.add(spans[1].classList);
+                            spans[1].classList.remove(spans[1].classList);
+                            spans[1].classList.add(temp);
+                        }
+                    });
+                });
+            }
+            if (buttonFound.classList.contains('changeable')) {
+                if (buttonFound.classList.contains('lang')) {
+                    let span = buttonFound.querySelector('span.down');
+                    textarea.value += span.textContent;
+                }
+            }
+            if (buttonFound.classList.contains('tab')) {
+                textarea.value += '        ';
+            }
+        }
+    });
+
+    pressed.add(event.key);
+    if (!pressed.has('Control') || !pressed.has('Alt')) {
+        return;
+    }
+    pressed.clear();
+
+    languageData = (languageData == 'rus') ? 'eng' : 'rus';
+    sessionStorage.setItem('language', languageData);
+    rowElements.forEach(rowElement => {
+        const buttonElements = [...rowElement.children];
+        buttonElements.forEach(buttonElement => {
+            if (buttonElement.classList.contains('change-lang')) {
+                buttonElement.classList.remove('change-lang');
+                buttonElement.classList.add('lang');
+            } else {
+                buttonElement.classList.remove('lang');
+                buttonElement.classList.add('change-lang');
+            }
+        });
+    });
+});
+
+document.addEventListener('keyup', event => {
+    const rowElements = [...keyboard.children];
+    rowElements.forEach(rowElement => {
+        const buttonElements = [...rowElement.querySelectorAll('.lang')];
+        let buttonFound = buttonElements.find(button => event.keyCode == button.dataset.keyCode);
+        if (buttonFound) {
+            if (event.code == 'ShiftRight') {
+                buttonFound = buttonElements.find(button => button.classList.contains('shift-right'));
+            } else if (event.code == 'AltRight') {
+                buttonFound = buttonElements.find(button => button.classList.contains('alt-right'));
+            } else if (event.code == 'ControlRight') {
+                buttonFound = buttonElements.find(button => button.classList.contains('ctrl-right'));
+            }
+            buttonFound.classList.remove('active');
+            if (buttonFound.dataset.keyCode == 16) {
+                isShift = false;
+                const rowElements = [...keyboard.children];
+                rowElements.forEach(rowElement => {
+                    const buttonElements = [...rowElement.querySelectorAll('.lang')];
+                    buttonElements.forEach(buttonElement => {
+                        if (buttonElement.classList.contains('changeable')) {
+                            const spans = [...buttonElement.children];
+                            let temp = null;
+                            temp = spans[0].classList.value;
+                            spans[0].classList.remove(spans[0].classList.value);
+                            spans[0].classList.add(spans[1].classList);
+                            spans[1].classList.remove(spans[1].classList);
+                            spans[1].classList.add(temp);
+                        }
+                    });
+                });
+            }
+        }
+    });
+
+    pressed.delete(event.key);
+});
+
+textarea.onkeypress = event => {
+    if (!(event.keyCode == 32 || event.keyCode == 13)) {
+        return false;
+    }
+};
